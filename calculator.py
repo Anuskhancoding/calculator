8be5bb70f9ae3915cbbe31de3d23e85341c807aa
Anuskhancoding <anus2021@namal.edu.pk>
2023-10-09 04:44:02 -0700

    initial commit

--- calculator.py
+++ calculator.py
@@ -10,16 +10,58 @@ def add():
             break
     return result
 def sub():
-    result=0
+    result=int(input('enter number from which you want to subtract'))
     while True:
         a=int(input('Enter number'))
-        result-=a
+        result=result-a
         Check=True
         to_get_result=input("press small y to get result")
         if to_get_result=='y':
             Check=False
             break
     return result
-# print("Result is",add())
-print("result is",sub())
-
+def mul():
+    result=1
+    while True:
+        a=int(input('Enter number'))
+        result=result*a
+        Check=True
+        to_get_result=input("press small y to get result")
+        if to_get_result=='y':
+            Check=False
+            break
+    return result
+def div():
+    result=0
+    a=int(input('Enter qoutient'))
+    b=int(input('enter divisor'))
+    result=a/b
+    return result
+def modolus():
+    result=0
+    a=int(input('Enter qoutient'))
+    b=int(input('enter divisor'))
+    result=a%b
+    return result
+while True:
+    print('1 to do add')
+    print('2 to do subtract')
+    print('3 to do multiply')
+    print('4 to do div')
+    print('5 to do modulous')
+    print('6 to exit')
+    choice=int(input('enter choice what you want to do'))
+    if choice ==1:
+        print("result",add())
+    if choice == 2:
+        print("result",sub())
+    if choice==3:
+        print("result",mul())
+    if choice==4:
+        print("result",div())
+    if choice==5:
+        print("result",modolus())
+    if choice==6:
+        break
+    else:
+        print('enter valid number')
